ed200217aeeeea65037c01f81f4f4782428adc87
putramuttaqin <putramuttaqin@gmail.com>
2025-07-09 10:01:48 +0700

    rename route to be more intiutive

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { app, security, storage } = require('./config/server-config');
-const documentRoutes = require('./routes/permohonan');
+const documentRoutes = require('./routes/surat');
 
 const server = express();
 
