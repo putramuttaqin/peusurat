8245d96d25cec9d191430d60b9ca6e225203e27f
putramuttaqin <putramuttaqin@gmail.com>
2025-07-16 09:21:13 +0700

    Separate build for client and server

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: 'esbuild',
+  },
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
+  },
+});
