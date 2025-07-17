44a8be0cd1d6c41b4bdfbcce5d1b7b553c54ad02
putramuttaqin <putramuttaqin@gmail.com>
2025-07-17 12:09:45 +0700

    remove csv related file

--- server/src/routes/surat/index.js
+++ server/src/routes/surat/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const submissionRoutes = require('./submission.routes');
 const entriesRoutes = require('./entries.routes');
-const { initCsvFile } = require('../utils/csvUtils');
-
-// Initialize CSV file on startup
-initCsvFile();
 
 // Mount all routes
 router.use('/submit', submissionRoutes);
