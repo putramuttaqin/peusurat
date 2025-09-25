e48e47c3909d742baa194c8cbfc88b580fe32350
Putra M <putramuttaqin@gmail.com>
2025-09-25 09:31:33 +0700

    change auth error msg

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -4,14 +4,15 @@ const { security } = require('../config/server-config');
 
 function requireAuth(req, res, next) {
   const token = req.cookies.token;
-  if (!token) return res.status(401).json({ success: false, message: 'Unauthenticated' });
+  console.log(!token);
+  if (!token) return res.status(401).json({ success: false, message: 'Login Kadaluarsa, Mohon Login Kembali' });
 
   try {
     const user = jwt.verify(token, security.jwt.secret);
     req.user = user;
     next();
   } catch (err) {
-    return res.status(403).json({ success: false, message: 'Invalid token' });
+    return res.status(403).json({ success: false, message: 'Mohon Login Ulang' });
   }
 }
 
